737cc58cf091bb2d30ac8e3ec832aedfadc9ebe8
Pmnmpr <pmnmpr@gmail.com>
2022-12-10 16:10:42 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             63920a4f7c184             |
+    |             6394af54b2b68             |
     |_______________________________________|
 */
  use Pmpr\Module\Rating\Rating; Rating::symcgieuakksimmu();
--- src/Pmpr/Module/Rating/Model/Model.php
+++ src/Pmpr/Module/Rating/Model/Model.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             63920a4f7c184             |
+    |             6394af54b2b68             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Rating\Model; use Pmpr\Module\Rating\Container; class Model extends Container { public function aqyikqugcomoqqqi() { Rate::symcgieuakksimmu(); Feedback::symcgieuakksimmu(); } }
