905cdcc69e510540d31547b6eabe888b5a62e9cc
Pmnmpr <pmnmpr@gmail.com>
2023-01-24 14:37:33 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             63ccf3b51eed1             |
+    |             63cfed0069254             |
     |_______________________________________|
 */
  use Pmpr\Module\Rating\Rating; Rating::symcgieuakksimmu();
