c219e22ab02c5e35d90f78b5280c042b522952bd
Pmnmpr <pmnmpr@gmail.com>
2023-03-01 11:08:36 +0000

    Obfuscated

--- src/Pmpr/Module/Rating/StructuredData.php
+++ src/Pmpr/Module/Rating/StructuredData.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             63fe76b56195e             |
+    |             63ff320fdf9e9             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Rating; use Pmpr\Module\Rating\Model\Rate; use Pmpr\Module\StructuredData\AbstractStructuredData; use Pmpr\Module\StructuredData\Schema\CreativeWork\CreativeWorkSeries; use Pmpr\Module\StructuredData\Schema\Intangible\Rating\AggregateRating; class StructuredData extends AbstractStructuredData { public function cquokmemekqqywgi() { $sqsckcmykaiamcak = Rating::symcgieuakksimmu(); $seumokooiykcomco = $this->caokeucsksukesyo()->ayueggmoqeeukqmq(); $post = $seumokooiykcomco->get(); if (!($post && $sqsckcmykaiamcak->uiqcwsowwswommka($post))) { goto gygawoqywkukmqee; } $mcieuoqmagekmouk = Rate::symcgieuakksimmu(); $qqyuqswckkcomeak = $mcieuoqmagekmouk->ewmegmsaoeuyuuuq($post); if (!isset($qqyuqswckkcomeak[self::qiyqwyiiwykeccmo], $qqyuqswckkcomeak[self::yewgggaqoacewaai])) { goto sycygoiaiqqageym; } $ksoygooogqsikqmw = (new CreativeWorkSeries())->usuqmwksoeaayaig($seumokooiykcomco->qcgakseyaikigqco($post))->nsqwikmawcqqyayk((new AggregateRating())->oyigwesguooeqkmo(5)->agumqmayuckgugae(0)->aqgscesisoeawose($qqyuqswckkcomeak[self::qiyqwyiiwykeccmo])->squsoiakemiuoouq($qqyuqswckkcomeak[self::yewgggaqoacewaai])); $this->yuaukuikuewwqumk($ksoygooogqsikqmw); sycygoiaiqqageym: gygawoqywkukmqee: } }
