88f65fa4ce4e80d5200bd0e8fafbb9d2ac26a77f
pmpr.dev <pmpr.dev@gmail.com>
2024-03-29 10:34:58 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             65e5105e5aa35             |
+    |             6606992919432             |
     |_______________________________________|
 */
  use Pmpr\Module\Rating\Rating; Rating::symcgieuakksimmu();
--- src/Pmpr/Module/Rating/Model/Model.php
+++ src/Pmpr/Module/Rating/Model/Model.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             65e5105e5aa35             |
+    |             6606992919432             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Rating\Model; use Pmpr\Module\Rating\Container; class Model extends Container { public function aqyikqugcomoqqqi() { Rate::symcgieuakksimmu(); Feedback::symcgieuakksimmu(); } }
