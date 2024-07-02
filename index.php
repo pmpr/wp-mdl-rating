997ecbd473b06e51928608ec9ea98f29627023f9
pmpr.dev <pmpr.dev@gmail.com>
2024-07-02 13:31:13 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6646a8b615ba6             |
+    |             668400fa252a8             |
     |_______________________________________|
 */
  use Pmpr\Module\Rating\Rating; Rating::symcgieuakksimmu();
